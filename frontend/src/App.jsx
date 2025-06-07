13e4e865c490e2bf30d6a4fa7ab01a92d7c11be6
Daniel <danmer@hotmail.com>
2025-06-07 09:18:10 -0400

    Typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -84,7 +84,7 @@ function App() {
     <div className="app">
       <h1>TO DO</h1>
       <div className="points-counter">
-       Total Points: {tasks.reduce((sum, t) => t.completed ? sum + t.points : sum, 0)}
+       Total Points: {tasks.reduce((sum, t) => t.completed ? sum + t.points : sum, 0)} {   }
         Lifetime Daily Points: {historySummary.completed} / {historySummary.possible}
       today: {historySummary.todayCompleted} / {historySummary.todayPossible}
         Today's Points: {historySummary.todayCompleted} / {historySummary.todayPossible}    
